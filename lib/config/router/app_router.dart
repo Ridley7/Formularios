848974507ef3dceb019a7271274b25c94f136911
Ridley <gonzalezfranks@hotmail.com>
2026-03-30 09:47:09 +0200

    add: UI y fichero de rutas

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:formulario_flutter/presentation/screens/bloc_counter_screen.dart';
+import 'package:formulario_flutter/presentation/screens/cubit_counter_screen.dart';
+import 'package:formulario_flutter/presentation/screens/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(
+  routes: [
+
+    GoRoute(
+        path: '/',
+      builder: (context, state) => const HomeScreen()
+    ),
+
+    GoRoute(
+      path: '/cubits',
+      builder: (context, state) => CubitCounterScreen()
+    ),
+
+    GoRoute(
+      path: '/counter-bloc',
+      builder: (context, state) => BlocCounterScreen()
+    )
+
+  ]
+);
---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cubit Counter"),
+        actions: [
+          IconButton(
+              onPressed: () => {},
+              icon: const Icon(Icons.refresh_outlined)
+          )
+        ],
+      ),
+
+      body: const Center(
+        child: Text("Counter value:"),
+      ),
+
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+              onPressed: (){},
+            child: Text("+3"),
+          ),
+
+          const SizedBox(
+            height: 15,
+          ),
+
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: (){},
+            child: Text("+2"),
+          ),
+
+          const SizedBox(
+            height: 15,
+          ),
+
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: (){},
+            child: Text("+1"),
+          ),
+
+        ],
+      ),
+
+    );
+  }
+}
